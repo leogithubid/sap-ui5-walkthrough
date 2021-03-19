071493d83aee56fad049c1ac1c3df3d6c2ea151f
leo <svliyon@gmail.com>
2021-03-19 13:51:16 +1100

    step2-bootstrap
    step2-bootstrap

---
+++ webapp/index.js
@@ -0,0 +1,12 @@
+/* Sap.ui.define([
+    'require',
+    'dependency'
+], function(require, factory) {
+    'use strict';
+    
+}); */
+sap.ui.define([
+], function() {
+    'use strict';
+    alert('Bootstrap Paradox');
+});
