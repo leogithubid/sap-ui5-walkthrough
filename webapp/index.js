354438911e7199bd3381b715d3f08773dd3c80c1
leo <svliyon@gmail.com>
2021-03-23 13:24:58 +1100

    3-controls
    3-controls

--- webapp/index.js
+++ webapp/index.js
@@ -1,4 +1,5 @@
-/* Sap.ui.define([
+/*just keeping this as a reference for sap.ui.define for the time being 
+Sap.ui.define([
     'require',
     'dependency'
 ], function(require, factory) {
@@ -6,7 +7,12 @@
     
 }); */
 sap.ui.define([
-], function() {
+    "sap/m/Text"
+], function(oText) {
     'use strict';
-    alert('Bootstrap Paradox');
+    //alert('Bootstrap Paradox');
+    new oText({
+        text:"Hello ! I am Text control"
+    }
+).placeAt("pageContent")
 });
